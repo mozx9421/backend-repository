82537719d235574e6fbb2626e832c07aa93a2592
mozx9421 <77826948+mozx9421@users.noreply.github.com>
2022-02-28 19:17:56 +0700

    update 2/28/22

--- editstock=1.php
+++ editstock=1.php
@@ -407,7 +407,7 @@ if (isset($_GET['logout'])) {
                                                                 data: data
                                                             },
                                                             success: function(data) {
-                                                                // console.log(data)
+                                                                console.log(data)
                                                                 if (data == 1) {
                                                                     alert('บันทึกสำเร็จ')
                                                                     window.location.replace('editstock.php')
